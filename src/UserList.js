b9011eb4cfb70083213ba159401fd1999eb0d61d
ElBlanc <wowjajou@gmail.com>
2022-01-06 22:37:10 +0100

    first commit

--- src/UserList.js
+++ src/UserList.js
@@ -1,14 +1,13 @@
+import axios from "axios";
 import React,{useEffect,useState} from 'react'
 import CardList from './CardList'
 
 const UserList = () => {
     const [users, setUsers] = useState([])
     useEffect(() => {
-       fetch('https://jsonplaceholder.typicode.com/users')
-       .then((response) =>response.json())
-       .then((json)=>setUsers(json))
-       .catch((err)=>console.log(err))
-    }, [])
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((response)=>{setUsers(response.data)} )
+        .catch( (error)=>console.log(error))});
     return (
         <div className="container">
             {
